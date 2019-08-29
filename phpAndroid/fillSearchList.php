1272fef0bb42e326aa2109fb01ea3b9729c71535
try-no-cry <39919696+try-no-cry@users.noreply.github.com>
2019-08-30 04:56:31 +0530

    Add files via upload

--- phpAndroid/fillSearchList.php
+++ phpAndroid/fillSearchList.php
@@ -7,9 +7,27 @@ $resArray=array();
 $response=array();
 
 // $sql=$_POST['sql'];
-// $sql=" SELECT S.imageUrl,S.location,S.contact,F.name 
-//               FROM stores S,foods F,relfoodstore R
-//               WHERE R.st "                                   //complete the query
+$searchQuery=$_POST['searchQuery'];
+// $searchQuery="food";
+$sql=" SELECT F.imageUrl ,S.location,S.contact,F.name AS foodName,S.name AS shopName,F.pricePerKg
+              FROM foods F  JOIN relfoodstore R
+              ON F.food_id=R.food_id
+              JOIN stores S 
+               ON R.store_id=S.store_id
+
+               WHERE F.name LIKE  '%$searchQuery%'
+                   OR S.name LIKE  '%$searchQuery%'
+                   OR S.location LIKE  '%$searchQuery%'
+                   OR S.contact LIKE  '%$searchQuery%'
+                   OR S.name  LIKE  '%$searchQuery%'
+                   OR F.pricePerKg LIKE  '%$searchQuery%'
+               
+               "; 
+
+               //add any other search property for added columns
+           
+                  ;  
+
 
 if($stmt=$conn->prepare($sql)){
 
@@ -21,7 +39,26 @@ if($stmt=$conn->prepare($sql)){
     while($row=$result->fetch_assoc()){
 
 
-        $resArray['']=$row[''];
+        $resArray['imageUrl']=$row['imageUrl'];
+        $resArray['location']=$row['location'];
+        $resArray['contact']=$row['contact'];
+        $resArray['foodName']=$row['foodName'];
+        
+
+
+        $res[]=$resArray;
     }
+
+    $stmt->free_result();
+    $stmt->close();
+
+    $response["success"]=1;
+    $response["data"]=$res;
 }
 
+else {
+    $response["success"]=0;
+    $response["data"]=mysqli_error($conn);
+}
+
+echo json_encode($response);
